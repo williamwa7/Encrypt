259c490e589707011da1c56131ec18b8e692c0e8
William Alves <122879475+williamwa7@users.noreply.github.com>
2024-02-09 15:11:15 +0000

    adicionando opção de chaves

--- script.js
+++ script.js
@@ -1,86 +1,79 @@
 const textArea = document.querySelector(".text-area");
 const mensagem = document.querySelector(".mensagem");
+let keys = [];
+console.log(keys);
 
-// As "chaves" de criptografia que utilizaremos são:
-// ` letra "e" é convertida para "enter"`
-// `A letra "i" é convertida para "imes"`
-// `A letra "a" é convertida para "ai"`
-// `A letra "o" é convertida para "ober"`
-// `A letra "u" é convertida para "ufat"`
+function togglePasswordVisibility(inputId) {
+  const input = document.getElementById(inputId);
+  const button = document.querySelector(
+    `[onclick="togglePasswordVisibility('${inputId}')"]`
+  );
 
-function btnEncriptar() {
-  const textoEncriptado = encriptar(textArea.value);
-  mensagem.value = textoEncriptado;
+  if (input.type === "password") {
+    input.type = "text";
+    button.innerHTML = '<i class="fas fa-eye-slash"></i>'; // Altera o ícone para olho com barra
+    input.setAttribute("data-visible", "true"); // Define o atributo personalizado para indicar que a senha está visível
+  } else {
+    input.type = "password";
+    button.innerHTML = '<i class="fas fa-eye"></i>'; // Altera o ícone para olho normal
+    input.setAttribute("data-visible", "false"); // Define o atributo personalizado para indicar que a senha está oculta
+  }
+}
+
+// function btnEncriptar(id) {
+//   console.log("id", id);
+//   if (keys.length === 0) {
+//     alert("Por favor, importe as chaves antes de criptografar.");
+//   } else {
+//     const textoEncriptado = encriptar(textArea.value);
+//     mensagem.value = textoEncriptado;
+//     textArea.value = "";
+//   }
+// }
+
+// function btnDesencriptar() {
+//   if (keys.length === 0) {
+//     alert("Por favor, importe as chaves antes de descriptografar.");
+//   } else {
+//     const textoDesencriptado = desencriptar(textArea.value);
+//     mensagem.value = textoDesencriptado;
+//     textArea.value = "";
+//   }
+// }
+
+function toggleButtonCryptography(action) {
+  let stringToProcess = textArea.value;
+  if (keys.length === 0) {
+    alert("Por favor, importe as chaves antes!");
+    return;
+  }
+
+  let matrizCodigo = keys;
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    const from =
+      action === "btnEncriptar" ? matrizCodigo[i][0] : matrizCodigo[i][1];
+    const to =
+      action === "btnEncriptar" ? matrizCodigo[i][1] : matrizCodigo[i][0];
+    stringToProcess = stringToProcess.replaceAll(from, to);
+  }
+
+  mensagem.value = stringToProcess;
   textArea.value = "";
 }
 
-function encriptar(stringEncriptada) {
-  let matrizCodigo = [
-    ["e", "enter"],
-    ["i", "imes"],
-    ["a", "ai"],
-    ["o", "ober"],
-    ["u", "ufat"],
+// function encriptar(stringEncriptada) {
+//   let matrizCodigo = keys;
+//   console.log("Encrypt Matriz:", matrizCodigo);
 
-  ];
-  console.table(matrizCodigo);
+//   //stringEncriptada = stringEncriptada.toLowerCase();
+// }
 
-  //stringEncriptada = stringEncriptada.toLowerCase();
+// function desencriptar(stringDesencriptada) {
+//   let matrizCodigo = keys;
+//   console.log("Decrypt Matriz:", matrizCodigo);
 
-  for (let i = 0; i < matrizCodigo.length; i++) {
-    if (stringEncriptada.includes(matrizCodigo[i][0])) {
-      stringEncriptada = stringEncriptada.replaceAll(
-        matrizCodigo[i][0],
-        matrizCodigo[i][1]
-      );
-    }
-  }
-
-  return stringEncriptada;
-}
-
-function btnDesencriptar() {
-  const textoDesencriptado = desencriptar(textArea.value);
-  mensagem.value = textoDesencriptado;
-  textArea.value = "";
-}
-
-function desencriptar(stringDesencriptada) {
-  let matrizCodigo = [
-    ["e", "enter"],
-    ["i", "imes"],
-    ["a", "ai"],
-    ["o", "ober"],
-    ["u", "ufat"],
-  ];
-  //stringDesencriptada = stringDesencriptada.toLowerCase();
-
-  for (let i = 0; i < matrizCodigo.length; i++) {
-    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
-      stringDesencriptada = stringDesencriptada.replaceAll(
-        matrizCodigo[i][1],
-        matrizCodigo[i][0]
-      );
-    }
-  }
-
-  return stringDesencriptada;
-}
-
-/* -------------outra opcão para copiar o conteúdo------------
-function copiar() {
-  var copiado = document.getElementById("idUnico").value;
-
-  if (navigator.clipboard.writeText(copiado)) {
-    document.getElementById("btnCopiar").textContent = "Copiado ✓";
-  }
-
-  setInterval(function () {
-    document.getElementById("btnCopiar").textContent = "Copiar";
-  }, 3000);
-}
------------------------------------------------------------------
-*/
+//   //stringDesencriptada = stringDesencriptada.toLowerCase();
+// }
 
 //código a seguir é para copiar e colar o conteúdo//
 //fonte: https://youtu.be/r2f1v7KrCsM//
@@ -115,3 +108,75 @@ btn_Colar.addEventListener("click", async (e) => {
     document.getElementById("btnColar").textContent = "Colar";
   }, 3000);
 });
+
+function generateKeys() {
+  alert("Atenção! As chaves geradas são únicas. Guarde-as bem, pois você não poderá recuperá-las.");
+  // Função para gerar um conjunto aleatório de 5 letras
+  function gerarConjuntoAleatorio() {
+    const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Apenas letras maiúsculas
+    let conjunto = "";
+    for (let i = 0; i < 5; i++) {
+      // Vamos fazer um loop para pegar 5 caracteres
+      const indiceAleatorio = Math.floor(Math.random() * caracteres.length); // Pegar um índice aleatório no conjunto de caracteres
+      conjunto += caracteres.charAt(indiceAleatorio); // Adicionar o caractere correspondente ao conjunto
+    }
+    return conjunto;
+  }
+
+  // Lista de caracteres
+  // const caracteres = "abcdefghijklmnopqrstuvwxyzáéíóúâêîôûàèìòùãõäëïöüçñ0123456789!@#$%&* ";
+  const caracteres =
+    "><:4o-2r[bò!i'19t8*.ê6mé|#zì@{lãí%+u7x`a/$^fû;_e?0dsj5ùóhè,nq}kõyvg]c3âwàáî&)(=ô~púõ ";
+  // Array de pares de caracteres e códigos para encript
+  const encrypt = caracteres
+    .split("")
+    .map((caractere) => [caractere, gerarConjuntoAleatorio()]);
+
+  // Converter para formato JSON
+  const jsonData = JSON.stringify(encrypt, null, 2);
+
+  // Criar um objeto Blob contendo o JSON
+  const blob = new Blob([jsonData], { type: "application/json" });
+
+  // Criar um link de download para o arquivo
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "encryptKeys.json";
+
+  // Simular um clique no link de download
+  document.body.appendChild(a);
+  a.click();
+
+  // Limpar o objeto URL
+  URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+}
+
+async function importKeys() {
+  const input = document.createElement("input");
+  input.type = "file";
+
+  input.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (event) {
+      const jsonData = event.target.result;
+      keys = JSON.parse(jsonData);
+      console.log("Chaves importadas:", keys);
+
+      // Aqui você pode fazer o que precisar com as chaves do arquivo JSON
+
+      const btnImportar = document.getElementById("btnImportar");
+      btnImportar.textContent = "Importado ✓";
+      btnImportar.disabled = true;
+      btnImportar.style.opacity = "0.4";
+      btnImportar.style.cursor = "not-allowed";
+    });
+
+    reader.readAsText(file);
+  });
+
+  input.click();
+}
